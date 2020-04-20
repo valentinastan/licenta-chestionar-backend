b0aad1fe4cee705c3b16321e955b809a46ef0bf7
valentinastan <valentinaa.stan98@gmail.com>
2020-04-20 13:10:45 +0300

    allow options for cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(morgan)
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 })
